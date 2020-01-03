354a0719903e3232751e8769ec23a7fbe134842e
Himmelt <master@void-3.cn>
2020-01-03 21:47:11 +0800

    update ama0801

--- VirtualDevice/Ama0801.cs
+++ VirtualDevice/Ama0801.cs
@@ -17,11 +17,13 @@ namespace virtualdevice {
         // 内部变量
         private bool start = false; // A0.0 start
         private bool jogCW = false; // A0.1 jog cw
+
         private bool jogCCW = false; // A0.2 jog ccw
+
         // private bool mode_0 = false; // A0.3 mode 2^0
         // private bool mode_1 = false; // A0.4 mode 2^1
         // private bool mode_2 = false; // A0.5 mode 2^2
-        private Mode _mode = Mode.Jog;
+        private int mode = -1;
         private bool synchronize = false; // A0.6 synchronize offset
         private bool softLimit = false; // A0.7 software limit switch off
         private bool inhibit = false; // A1.0 controller inhibit/enable
@@ -49,6 +51,7 @@ namespace virtualdevice {
         private readonly int[] _posValues = new int[16];
         private readonly int[] _camValues = new int[16];
 
+        private long lastTick = 0;
         private int actualValue = 0;
         private double _actualPos = 0.0;
 
@@ -66,6 +69,11 @@ namespace virtualdevice {
         private readonly bool[] _signals = new bool[16];
         private readonly bool[] _sensors = new bool[16];
         private readonly bool[] _cameras = new bool[16];
+
+        private const byte Jog = 0b_0000_0000;
+        private const byte Pos = 0b_0001_0000;
+        private const byte Ref = 0b_0010_0000;
+        private const byte Tech = 0b_0010_1000;
 
         public Ama0801(string name, int addr) : base(name) {
             _addr = addr;
@@ -101,15 +109,23 @@ namespace virtualdevice {
         }
 
         public override void Read(byte[] outBytes) {
-            inhibit = Io.ReadBit(outBytes, _addr + 1, 0);
-            notEStop = Io.ReadBit(outBytes, _addr + 1, 1);
-            notStop = Io.ReadBit(outBytes, _addr + 1, 2);
-            start = Io.ReadBit(outBytes, _addr + 0, 0);
-            _mode = (Mode) (outBytes[_addr + 0] & 0b_0011_1000);
-            targetPos = Io.GetOneIndex(Io.ReadShort(outBytes, _addr + 3));
-            fastMode = Io.ReadBit(outBytes, _addr + 1, 7);
-            jogCw = Io.ReadBit(outBytes, _addr + 0, 1);
-            jogCcw = Io.ReadBit(outBytes, _addr + 0, 2);
+            start     = Io.ReadBit(outBytes, _addr + 0, 0);
+            jogCw     = Io.ReadBit(outBytes, _addr + 0, 1);
+            jogCcw    = Io.ReadBit(outBytes, _addr + 0, 2);
+            inhibit   = Io.ReadBit(outBytes, _addr + 1, 0);
+            notEStop  = Io.ReadBit(outBytes, _addr + 1, 1);
+            notStop   = Io.ReadBit(outBytes, _addr + 1, 2);
+            fastMode  = Io.ReadBit(outBytes, _addr + 1, 7);
+
+            mode = outBytes[_addr + 0] & 0b_0011_1000;
+
+            targetPos = Io.seekOne(outBytes[_addr + 3]);
+            if (targetPos == -1) {
+                targetPos = Io.seekOne(outBytes[_addr + 4]);
+                if (targetPos != -1) {
+                    targetPos += 8;
+                }
+            }
         }
 
         public override void Write(byte[] inBytes) {
@@ -128,100 +144,64 @@ namespace virtualdevice {
             Io.WriteBits(inBytes, _addr + 9, _cameras); // ED 9
         }
 
-        /// <summary>
-        /// AMA_A
-        /// inhibit    : BOOL;// A1.0 controller inhibit/enable
-        /// notEStop   : BOOL;// A1.1 enable/rapid stop
-        /// notStop    : BOOL;// A1.2 enable/stop
-        /// reserve1   : BOOL;// A1.3 reserved
-        /// rampScaling: BOOL;// A1.4 ramp scaling
-        /// reserve2   : BOOL;// A1.5 reserved
-        /// reset      : BOOL;// A1.6 reset
-        /// fastMode   : BOOL;// A1.7 fast fast/slow fast
-        /// //////////////////////////////////////////////////////
-        /// start      : BOOL;// A0.0 start
-        /// jogCW      : BOOL;// A0.1 jog cw
-        /// jogCCW     : BOOL;// A0.2 jog ccw
-        /// mode_0     : BOOL;// A0.3 mode 2^0
-        /// mode_1     : BOOL;// A0.4 mode 2^1
-        /// mode_2     : BOOL;// A0.5 mode 2^2
-        /// synchronize: BOOL;// A0.6 synchronize offset
-        /// softLimit  : BOOL;// A0.7 software limit switch off
-        /// ///////////////////////////////////////////////////////
-        /// targetPos1 : BOOL;// A3.0 single bit position 1
-        /// targetPos2 : BOOL;// A3.1 single bit position 2
-        /// targetPos3 : BOOL;// A3.2 single bit position 3
-        /// targetPos4 : BOOL;// A3.3 single bit position 4
-        /// </summary>
         public override void Run() {
-            if (_k100 && !inhibit && notEStop && notStop) {
-                if (start && _mode == Mode.Pos) {
+            var tick =  DateTime.Now.Ticks;
+            if (tick - lastTick >= 1000 && _k100 && !inhibit && notEStop && notStop) {
+                lastTick = tick;
+                if (start && mode == Pos) {
                     var speed = fastMode ? _speed : (_speed * 0.1);
 
                     if (targetPos < 0 || targetPos >= 16) return;
                     var targetValue = _posValues[targetPos];
 
-                    if (_actualPos > _leftLimit && _actualPos < _rightLimit &&
-                        !(Math.Abs(_actualPos - targetValue) < 0.01)) {
+                    if (_actualPos > _leftLimit && _actualPos < _rightLimit && !(Math.Abs(_actualPos - targetValue) < 0.01)) {
                         if (_actualPos - targetValue > speed) {
                             _actualPos -= speed;
-                        }
-                        else if (targetValue - _actualPos > speed) {
+                        } else if (targetValue - _actualPos > speed) {
                             _actualPos += speed;
-                        }
-                        else {
+                        } else {
                             _actualPos = targetValue;
                         }
 
                         _axisSync = true;
-                    }
-                    else {
+                    } else {
                         return;
                     }
-                }
-                else if (_mode == Mode.Jog) {
+                } else if (mode == Jog) {
                     var speed = _speed * 0.01;
                     if (jogCw) {
                         _actualPos += speed;
                         _axisSync = true;
-                    }
-                    else if (jogCcw) {
+                    } else if (jogCcw) {
                         _actualPos -= speed;
                         _axisSync = true;
                     }
                 }
-            }
 
-            actualValue = (int) Math.Round(_actualPos);
+                actualValue = (int) Math.Round(_actualPos);
+                _beRef = actualValue > -_offset && actualValue < _offset;
+                inPosition = false;
+                for (var i = 0; i < 16; i++) {
+                    _signals[i] = Math.Abs(actualValue - _posValues[i]) <= _window && (targetPos == i || !start);
+                    _sensors[i] = Math.Abs(actualValue - _posValues[i]) <= _offset;
+                    _cameras[i] = Math.Abs(actualValue - _camValues[i]) <= _window;
+                    inPosition = inPosition || _signals[i];
+                }
+
+                limitCw = actualValue >= _rightLimit;
+                limitCcw = actualValue <= _leftLimit;
+            }
 
             // FINISH
             _k100K = _k100;
             _nK100K = !_k100;
-            _beRef = actualValue > -_offset && actualValue < _offset;
             _qkRxK = false;
             _fqm = true;
             _fqr = true;
             _bbm = true;
             _bbr = true;
-            inPosition = false;
-            for (var i = 0; i < 16; i++) {
-                _signals[i] = Math.Abs(actualValue - _posValues[i]) <= _window && (targetPos == i || !start);
-                _sensors[i] = Math.Abs(actualValue - _posValues[i]) <= _offset;
-                _cameras[i] = Math.Abs(actualValue - _camValues[i]) <= _window;
-                inPosition = inPosition || _signals[i];
-            }
-
             invReady = _k100;
             notBreak = start;
-            limitCw = actualValue >= _rightLimit;
-            limitCcw = actualValue <= _leftLimit;
         }
-    }
-
-    internal enum Mode {
-        Jog = 0b_0000_0000,
-        Pos = 0b_0001_0000,
-        Ref = 0b_0010_0000,
-        Tech = 0b_0010_1000
     }
 }
--- VirtualDevice/Io.cs
+++ VirtualDevice/Io.cs
@@ -1,51 +1,36 @@
 ﻿namespace virtualdevice {
     internal static class Io {
         public static bool ReadBit(byte[] data, int index, byte bit) {
-            if (index >= 0 && index < data.Length && bit < 8) {
-                return (data[index] & (0x01 << bit)) != 0;
-            }
-
-            return false;
+            return (data[index] & (0x01 << bit)) != 0;
         }
 
         public static void WriteBit(byte[] data, int index, byte bit, bool flag) {
-            if (index >= 0 && index < data.Length && bit < 8) {
-                data[index] = flag ? (byte) (data[index] | (0x01 << bit)) : (byte) (data[index] & ~(0x01 << bit));
-            }
+            data[index] = flag ? (byte) (data[index] | (0x01 << bit)) : (byte) (data[index] & ~(0x01 << bit));
         }
 
         public static void WriteBits(byte[] data, int index, bool[] values) {
-            var len = (values.Length + 7) / 8;
-            if (index >= 0 && index + len < data.Length) {
-                for (var i = 0; i < values.Length; i++) {
-                    var offset = i / 8;
-                    data[index + offset] = values[i]
-                        ? (byte) (data[index + offset] | (0x01 << i % 8))
-                        : (byte) (data[index + offset] & ~(0x01 << i % 8));
-                }
+            for (var i = 0; i < values.Length; i++) {
+                var offset = i / 8;
+                data[index + offset] = values[i]
+                    ? (byte) (data[index + offset] | (0x01 << i % 8))
+                    : (byte) (data[index + offset] & ~(0x01 << i % 8));
             }
         }
 
         public static short ReadShort(byte[] data, int index) {
-            if (index >= 0 && index < data.Length - 1) {
-                short value = data[index];
-                return (short) (value << 8 + data[index + 1]);
-            }
-
-            return 0x00;
+            int value = data[index];
+            return (short) (value << 8 + data[index + 1]);
         }
 
         public static void WriteInt(byte[] data, int index, int value) {
-            if (index >= 0 && index < data.Length - 4) {
-                data[index] = (byte) (value & 0xff);
-                data[index + 1] = (byte) (value >> 8 & 0xff);
-                data[index + 2] = (byte) (value >> 16 & 0xff);
-                data[index + 3] = (byte) (value >> 24 & 0xff);
-            }
+            data[index] = (byte) (value & 0xff);
+            data[index + 1] = (byte) (value >> 8 & 0xff);
+            data[index + 2] = (byte) (value >> 16 & 0xff);
+            data[index + 3] = (byte) (value >> 24 & 0xff);
         }
 
-        public static int GetOneIndex(short value) {
-            for (var i = 0; i < 16; i++) {
+        public static sbyte seekOne(byte value) {
+            for (sbyte i = 0; i < 8; i++) {
                 value >>= 1;
                 if (value != 0) {
                     continue;
@@ -55,14 +40,6 @@
             }
 
             return -1;
-        }
-
-        public static byte ReadByte(byte[] data, int index, byte mask) {
-            if (index >= 0 && index < data.Length) {
-                return (byte) (data[index] & mask);
-            }
-
-            return 0x00;
         }
     }
 }
